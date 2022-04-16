8ce6d26ce512fadfefb845d46ab9fcd8abd7858d
HabiburRahmanAnik <habib264521@gmail.com>
2022-04-17 00:07:34 +0600

    add background image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import classes from './Home.module.css';
-import background from '../assects/images/banner1.jpg';
 
 const Home = () => {
   return (
-    <div>
-      <section className={classes.hero}>
-        <div>
-          <div
-            style={{
-              backgroundImage: `url("https://i.ibb.co/K79K2wZ/banner2.jpg")`,objectFit:'cover'
-            }}
-          >
-            Hello World
-          </div>
-        </div>
-      </section>
-    </div>
+    <section className={classes.hero}>
+      <div className={classes.container}></div>
+      <div className={classes.content}>
+        <h1 className={classes.heading}>
+          <p>Keep your body</p>
+          <p className={classes.color}>fit and strong</p>
+        </h1>
+      </div>
+    </section>
   );
 };
 
